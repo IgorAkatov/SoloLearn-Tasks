e07ecb065b7c5410c213abfbba22dc26534f5958
Igor Akatov <156123230+IgorAkatov@users.noreply.github.com>
2024-01-28 23:43:15 +0300

    Update license.cs

--- Hard/New Driver's License/license.cs
+++ Hard/New Driver's License/license.cs
@@ -1,1 +1,15 @@
+string myName = Console.ReadLine()!;
+int agents = Convert.ToInt32(Console.ReadLine());
+string[] costumers = Console.ReadLine()!.Split(' ');
 
+//count считает количество человек перед тобой в очереди
+int count = 0;
+for (int i = 0; i < costumers.Length; i++)
+{
+    count = string.Compare(myName, costumers[i]) > 0 ? ++count : count;
+}
+// получим группу, в которой ты зайдешь в кабинет
+int result = count / agents;
+// теперь время выхода твоей группы из кабинета
+result = result * 20 + 20;
+Console.WriteLine(result);
